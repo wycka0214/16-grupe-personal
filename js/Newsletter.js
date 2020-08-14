6a2d38d1209ad8767a3afd4254c1ebfb61c903c1
Rimantas <belauzas@yahoo.com>
2020-08-14 21:33:27 +0300

    Newsletter email validation tests

--- js/Newsletter.js
+++ js/Newsletter.js
@@ -5,6 +5,12 @@ class Newsletter {
         this.selector = params.selector;
         this.insertPosition = params.insertPosition;
         this.parentDOM = null;
+        this.DOM = null;
+        this.inputDOM = null;
+        this.buttonDOM = null;
+        this.messagesDOM = null;
+
+        this.email = '';
 
         this.init();
     }
@@ -37,12 +43,44 @@ class Newsletter {
     }
 
     addEvents() {
-        // TODO: click ant submit mygtuko
-        // TODO: enter input'e
+        // this.buttonDOM.addEventListener('click', this.submitForm.bind(this)); // alternatyva kai baindiname konteksta (en. context binding)
+        this.buttonDOM.addEventListener('click', () => {
+            this.submitForm();
+        });
+
+        this.inputDOM.addEventListener('keypress', (event) => {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                this.submitForm();
+            }
+        });
     }
 
-    validateInput() {
-        // TODO: validuojame email formata
+    submitForm() {
+        this.email = this.inputDOM.value;
+        if (!Newsletter.isValidEmail(this.email)) {
+            this.displayFormMessage();
+        }
+
+        this.sendRequest();
+    }
+
+    static isValidEmail(email) {
+        console.log('tikriname:', email);
+        /* Kriterijai validziam email adresui:
+        1. ilgis - min 7 simboliai (a@a1.lt)
+        2. ilgis - max 100
+        3. @ === 1
+        4. pries @ - bent 1 simbolis
+        5. uz @ - min 5 simboliai
+        6. taskas - bent vienas uz @
+        7. taskas - negali buti pirmas simbolis uz @
+        8. taskas - negali buti 2 is eiles (niekur)
+        9. taskas - gali buti trecias arba ketvirtas (arciausiai) nuo galo uz @
+        10. validus - tik [lotyniskos/angliskos raides, @, ., [0..9]]
+        */
+
+        return true;
     }
 
     displayFormMessage() {
@@ -67,6 +105,11 @@ class Newsletter {
         } else {
             this.parentDOM.innerHTML += HTML;
         }
+
+        this.DOM = this.parentDOM.querySelector('.form');
+        this.inputDOM = this.DOM.querySelector('input');
+        this.buttonDOM = this.DOM.querySelector('.btn');
+        this.messagesDOM = this.DOM.querySelector('.messages');
     }
 }
 
