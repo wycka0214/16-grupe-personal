77e0ac94c0779d682f406f7f01d4a1fd543c458d
Rimantas <belauzas@yahoo.com>
2020-08-17 19:14:53 +0300

    Email validation logic

--- js/Newsletter.js
+++ js/Newsletter.js
@@ -66,19 +66,95 @@ class Newsletter {
     }
 
     static isValidEmail(email) {
-        console.log('tikriname:', email);
-        /* Kriterijai validziam email adresui:
-        1. ilgis - min 7 simboliai (a@a1.lt)
-        2. ilgis - max 100
-        3. @ === 1
-        4. pries @ - bent 1 simbolis
-        5. uz @ - min 5 simboliai
-        6. taskas - bent vienas uz @
-        7. taskas - negali buti pirmas simbolis uz @
-        8. taskas - negali buti 2 is eiles (niekur)
-        9. taskas - gali buti trecias arba ketvirtas (arciausiai) nuo galo uz @
-        10. validus - tik [lotyniskos/angliskos raides, @, ., [0..9]]
-        */
+        // console.log('tikriname:', email);
+
+        // 0. tekstas
+        if (typeof email !== 'string') {
+            console.warn('ERROR: email turi buti tekstinis.');
+            return false;
+        }
+
+        const emailLength = email.length;
+
+        // 1. ilgis - min 7 simboliai (a@a1.lt)
+        if (emailLength < 7) {
+            console.warn('ERROR: email ne maziau 7 simboliu.');
+            return false;
+        }
+
+        // 2. ilgis - max 100
+        if (emailLength > 100) {
+            console.warn('ERROR: email ne daugiau 100 simboliu.');
+            return false;
+        }
+
+        // 3. @ === 1
+        let etaCount = 0;
+        let etaPosition = null;
+        for (let i = 0; i < emailLength; i++) {
+            if (email[i] === '@') etaCount++;
+            if (email[i] === '@' && etaPosition === null) etaPosition = i;
+        }
+        if (etaCount !== 1) {
+            console.warn('ERROR: email tik vienas @ simbolis.');
+            return false;
+        }
+
+        // 4. pries @ - bent 1 simbolis
+        if (email[0] === '@') {
+            console.warn('ERROR: email turi tureti local-part (tekstas priesais @ simboli).');
+            return false;
+        }
+
+        // 5. uz @ - min 5 simboliai
+        const symbolsAfterEta = emailLength - etaPosition - 1;
+        if (symbolsAfterEta < 5) {
+            console.warn('ERROR: email turi tureti domain-part is min 5 simboliu (uz @).');
+            return false;
+        }
+
+        // 6. taskas - bent vienas uz @
+        let dotCount = 0;
+        for (let i = 0; i < emailLength; i++) {
+            if (email[i] === '.') dotCount++;
+        }
+        if (dotCount === 0) {
+            console.warn('ERROR: email domain-part turi tureti bent viena taska.');
+            return false;
+        }
+
+        // 7. taskas - negali buti pirmas simbolis uz @
+        if (email[etaPosition + 1] === '.') {
+            console.warn('ERROR: email domain-part negali prasideti tasku.');
+            return false;
+        }
+
+        // 8. taskas - negali buti 2 is eiles (niekur)
+        if (email.includes('..')) {
+            console.warn('ERROR: email negali tureti dvieju is eiles einanciu tasku.');
+            return false;
+        }
+
+        // 9. taskas - gali buti trecias arba ketvirtas (arciausiai) nuo galo uz @
+        if (email[emailLength - 2] === '.' ||
+            email[emailLength - 1] === '.') {
+            console.warn('ERROR: taskas negali buti paskutinis arba pries paskutinis simbolis.');
+            return false;
+        }
+
+        // 10. validus - tik [lotyniskos/angliskos raides, @, ., [0..9]]
+        const lettersLower = 'qwertyuiopasdfghjklzxcvbnm';
+        const lettersUpper = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+        const numbers = '7894561230';
+        const symbols = '@.';
+        const whitelist = lettersLower + lettersUpper + numbers + symbols;
+
+        for (const letter of email) {
+            if (!whitelist.includes(letter)) {
+                console.warn(`ERROR: "${letter}" neleidzimas email adrese.`);
+                return false;
+            }
+        }
 
         return true;
     }
