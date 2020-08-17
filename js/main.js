e65db98b8bdd07e227e5a8c72504439b7dc1267b
Rimantas <belauzas@yahoo.com>
2020-08-17 19:18:42 +0300

    Email test iskelti i atskira faila

--- js/main.js
+++ js/main.js
@@ -11,29 +11,3 @@ new Newsletter({
     selector: '#footer_newsletter',
     insertPosition: 'beforeend'
 });
-
-
-
-
-console.log(Newsletter.isValidEmail(), '->', false);
-console.log(Newsletter.isValidEmail(58415), '->', false);
-console.log(Newsletter.isValidEmail('5151'), '->', false);
-console.log(Newsletter.isValidEmail('absdkaskdfakjndfl; ansdfjnasjkdfnkjasndjfknasjkdnfkjasndfjnasjkdfkasndfknaskdjfnasdnfakansddkfnaskjdndfasdf'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis.gmail.com'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@@gmail.com'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@pavardenis@gmail.com'), '->', false);
-console.log(Newsletter.isValidEmail('@gmail.com'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@a.lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@asddfa1lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@.a1lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@a1..lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis.......pavardenis@a1..lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis..pavardenis@a1.lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis..pavardenis@a1.lt'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@a1l.t'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis@a1lt.'), '->', false);
-console.log(Newsletter.isValidEmail('vardenis<>/*-+@a1.lt'), '->', false);
-
-console.log(Newsletter.isValidEmail('a@a1.lt'), '->', true);
-console.log(Newsletter.isValidEmail('vardenis.pavardenis@gmail.lt'), '->', true);
-console.log(Newsletter.isValidEmail('vardenis.pavardenis@gmail.com'), '->', true);
